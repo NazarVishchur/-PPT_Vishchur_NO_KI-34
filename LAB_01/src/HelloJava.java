dfa264faeae88217379c3b941f242317f30cc664
NazarVishchur <nazirko222@gmail.com>
2022-09-06 22:59:15 +0300

    commit

--- LAB_01/src/HelloJava.java
+++ LAB_01/src/HelloJava.java
@@ -5,7 +5,7 @@ class HelloJava
 	public static void main(String[] args) 
 	{
 			String message = new String();
-			message="Hello, Java";
+			message="Nazar Vishchur KI-34";
 			System.out.println(message);	
 	}
 
